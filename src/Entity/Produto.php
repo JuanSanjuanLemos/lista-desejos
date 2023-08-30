2811b60e63f0d55b9d146f1c362dd8e2d4f1cb41
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2023-08-30 17:19:43 -0300

    Create entity and repository Produto

---
+++ src/Entity/Produto.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProdutoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProdutoRepository::class)
+ */
+class Produto
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $link;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $valor;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isAdquirido;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getValor(): ?float
+    {
+        return $this->valor;
+    }
+
+    public function setValor(float $valor): self
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+
+    public function isIsAdquirido(): ?bool
+    {
+        return $this->isAdquirido;
+    }
+
+    public function setIsAdquirido(bool $isAdquirido): self
+    {
+        $this->isAdquirido = $isAdquirido;
+
+        return $this;
+    }
+}
