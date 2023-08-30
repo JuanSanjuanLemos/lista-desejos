b8dd564bbf52b4a8f8a1c47b64a350a0d6939d8b
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2023-08-30 17:44:45 -0300

    Create Categoria and Produto controller

---
+++ src/Controller/CategoriaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoriaController extends AbstractController
+{
+    /**
+     * @Route("/categoria", name="app_categoria")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CategoriaController.php',
+        ]);
+    }
+}
---
+++ src/Controller/ProdutoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProdutoController extends AbstractController
+{
+    /**
+     * @Route("/produto", name="app_produto")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProdutoController.php',
+        ]);
+    }
+}
